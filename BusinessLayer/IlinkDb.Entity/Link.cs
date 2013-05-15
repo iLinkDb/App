d4c6cfe0e7f6a15af2f4491c3628821cbb5f8552
Scott Brooks <scottb@tsbrooks.com>
2013-05-15 07:01:36 -0400

    Added Entity Project

---
+++ BusinessLayer/IlinkDb.Entity/Link.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlinkDb.Entity
+{
+    public class Link
+    {
+        public string Url { get; set; }
+    }
+}
