17f83e59997463e5dd45fa0502d67daa7f363daa
Scott Brooks <scottb@tsbrooks.com>
2013-06-21 06:06:04 -0400

    Working nice, before renaming itemOne to itemTenant

--- Presentation/IlinkDb.WebRole/Scripts/AppMain.js
+++ Presentation/IlinkDb.WebRole/Scripts/AppMain.js
@@ -3,7 +3,7 @@ if (typeof am == 'undefined' || !am) {
     am = {};
 }
 
-am.EditDialogHide = function (formName) {
+am.DialogHide = function (formName) {
     var form = $('#' + formName);
     form.validate().resetForm();
     form.get(0).reset();
@@ -11,13 +11,27 @@ am.EditDialogHide = function (formName) {
     form.find("input[type='hidden'][id='id']").remove();
 };
 
-am.PostEdit = function (form) {
+am.DialogShow = function (formName) {
+    var form = $('#' + formName);
+    var itemOne = form.data('itemOne');
+    if (!itemOne)
+        return;
+
+    $('<input>').attr('type', 'hidden')
+                .attr('id', 'id')
+                .attr('name', 'id')
+                .val(itemOne.id()).prependTo(form);
+    form.find('#Domain').val(itemOne.domain());
+}
+
+am.PostEdit = function (form, modalName) {
     form = $(form);
     //                if (!form.valid())
     //                    return;
     var json = JSON.stringify(am._getItemFromForm(form));
 
     console.log('In PostEdit');
+    console.dir(json);
 
     var update = form.find("input[type='hidden'][id='id']").val();
     var httpVerb = !update ? "POST" : "PUT";
@@ -30,8 +44,6 @@ am.PostEdit = function (form) {
         dataType: 'json',
         contentType: 'application/json; charset=utf-8',
         success: function (jsonObject) {
-            console.log('self');
-            console.dir(self);
             if (update) {
                 var match = ko.utils.arrayFirst(viewModel.itemList(), function (item) {
                     return jsonObject.id === item.id();
@@ -41,7 +53,32 @@ am.PostEdit = function (form) {
             else {
                 viewModel.itemList.push(new itemOne(jsonObject));
             }
-            $('#modalEdit').modal('hide');
+            $('#' + modalName).modal('hide');
+        }
+    });
+}
+
+am.PostDelete = function (form, modalName) {
+    form = $(form);
+    var json = JSON.stringify(this._getItemFromForm(form));
+
+    var deleteId = form.find("input[type='hidden'][id='id']").val();
+
+    var self = this;
+    $.ajax({
+        url: 'http://' + restHost + '/' + controller + '/delete',
+        type: 'DELETE',
+        data: json,
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        success: function (success) {
+            if (success) {
+                var match = ko.utils.arrayFirst(viewModel.itemList(), function (item) {
+                    return deleteId == item.id();
+                });
+                viewModel.itemList.remove(match);
+            }
+            $('#' + modalName).modal('hide');
         }
     });
 }
@@ -59,5 +96,10 @@ am.assert = function (value, desc) {
     var li = document.createElement("li");
     li.className = value ? "pass" : "fail";
     li.appendChild(document.createTextNode(desc));
-    document.getElementById("results").appendChild(li);
+    document.getElementById("assertResults").appendChild(li);
+}
+
+am.initModal = function (modalName) {
+    // Without this line, the dialog closes when somebody clicks outside of it.
+    $('#' + modalName).modal({ "backdrop": "static", show: false });
 }
